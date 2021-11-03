f53a12edff8fa5885938b9e6d9a646d014728d48
JerePalmu <jerepalmu03@gmail.com>
2021-11-03 15:03:44 +0200

    [slug].svelte korjauksia, kuvat ovat liian isoja sivun alussa, muuten aikalailla valmis.

--- src/routes/uutiset/[slug].svelte
+++ src/routes/uutiset/[slug].svelte
@@ -1,6 +1,10 @@
 <script>
 	import { postData } from "$lib/store";
 	import { onMount } from "svelte";
+	import { page } from '$app/stores';
+	import Image from "../../components/image.svelte";
+
+	const id = parseInt($page.params.slug);
 
 	let data = [];
 
@@ -10,3 +14,31 @@
 		});
 	});
 </script>
+
+{#if data && data.id}
+    <div id="images">
+
+		<ul class="grid grid-cols-1 gap-6"></ul>
+
+        {#each data.images as item}
+			<Image img={item} alt={data.title}/>		  
+        {/each}
+    </div>
+
+    <div class="content">{@html data.content}</div>
+{/if}
+
+<style scoped>
+	ul{
+	max-width: 200px;
+	max-height: 200px;
+}
+	.content{
+	display: block;
+	margin-left: 750px;
+	margin-right: 750px;
+	width: 50%;
+	margin-top: 5rem;
+	margin-bottom: 5rem;
+}
+</style>
