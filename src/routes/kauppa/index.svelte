bb1c751391aa844f23596e45e2a89c4e11b17993
JuhaHurri <juhah60@gmail.com>
2021-11-03 16:41:05 +0200

    Koodi
    Koodi ei toimi vielä yritin kaiken näköistä, mutta se ei toiminut vielä.

--- src/routes/kauppa/index.svelte
+++ src/routes/kauppa/index.svelte
@@ -5,18 +5,27 @@
 
 	// Tänne kerätään dataa
 	let products = [];
+	let priceCurrent = [];
 
 	// onMount ladataan kerran kun sivu / komponentti ladataan
 	onMount(async () => {
 		postData("products&imageWidth=348&imageHeight=420").then(function (result) {
-			data = result;
+			products = result;
 		});
 	});
+
+	onMount(async () => {
+		postData("priceCurrent").then(function (result) {
+			priceCurrent = result;
+		});
+	});
+
+	
 </script>
 
-{#if products && products[0]}
+{#if products.items && products.items[0]}
 	<ul class="grid grid-cols-3 gap-6">
-		{#each products as item}
+		{#each products.items as item}
 			<li>
 				<a
 					class="news rounded-b-md overlay-hidden"
@@ -39,3 +48,30 @@
 		{/each}
 	</ul>
 {/if}
+
+{#if priceCurrent && priceCurrent[0]}
+	<ul class="grid grid-cols-3 gap-6">
+		{#each priceCurrent as item}
+			<li>
+				<a
+					class="news rounded-b-md overlay-hidden"
+					href={"/kauppa/" + item.id}
+					title={"priceCurrent" + item.title}
+					rel="prefetch"
+				>
+					<figure>
+						<Image img={item.images[0]} alt={item.title} />
+						<figcaption
+							class="py-5 text-black text-lg italic leading-snug"
+						>
+							<strong>
+								{item.created} - {item.title}
+							</strong>
+						</figcaption>
+					</figure>
+				</a>
+			</li>
+		{/each}
+	</ul>
+{/if}
+
