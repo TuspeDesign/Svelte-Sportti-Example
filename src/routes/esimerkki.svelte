a55eaf752b1cc158e9f4280394e8d4108afd8128
timoanttila <14306628+timoanttila@users.noreply.github.com>
2021-11-03 10:41:33 +0200

    Uusi esimerkki

--- src/routes/esimerkki.svelte
+++ src/routes/esimerkki.svelte
@@ -4,22 +4,31 @@
 	import Image from "../components/image.svelte";
 
 	// Tänne kerätään dataa
-	let data = [];
+	let news = [];
+	let videos = [];
+	let games = [];
 
 	// onMount ladataan kerran kun sivu / komponentti ladataan
 	onMount(async () => {
 		postData("news&limit=4").then(function (result) {
-			data = result;
+			news = result;
+		});
+		postData("videos&limit=4").then(function (result) {
+			videos = result;
+		});
+		postData("games&limit=4").then(function (result) {
+			games = result;
 		});
 	});
 </script>
 
-{#if data && data[0]}
-	<ul class="grid grid-cols-3 gap-6">
-		{#each data as item}
+{#if news && news[0]}
+	<h2>Ajankohtaista</h2>
+	<ul class="grid sm:grid-cols-2 lg:grid-cols-4 gap-6">
+		{#each news as item}
 			<li>
 				<a
-					class="news rounded-b-md overlay-hidden"
+					class="news overlay-hidden"
 					href={"/uutiset/" + item.id}
 					title={"Uutinen: " + item.title}
 					rel="prefetch"
@@ -35,6 +44,63 @@
 						</figcaption>
 					</figure>
 				</a>
+			</li>
+		{/each}
+	</ul>
+{/if}
+
+{#if videos && videos[0]}
+	<h2>Uusimmat videot</h2>
+	<ul class="grid sm:grid-cols-2 lg:grid-cols-4 gap-6">
+		{#each videos as item}
+			<li>
+				<a
+					class="news overlay-hidden"
+					href={"/videot/" + item.id}
+					title={"Videot: " + item.title}
+					rel="prefetch"
+				>
+					<figure>
+						<Image img={item.images[0]} alt={item.title} />
+						<figcaption
+							class="py-5 text-white text-lg italic leading-snug"
+						>
+							<strong>
+								{item.created} - {item.title}
+							</strong>
+						</figcaption>
+					</figure>
+				</a>
+			</li>
+		{/each}
+	</ul>
+{/if}
+
+{#if games && games[0]}
+	<h2>Tulevat pelit</h2>
+	<ul>
+		{#each games as item}
+			<li>
+				<div>{item.date}</div>
+				<div class="grid grid-cols-3 gap-6">
+					<div>
+						<Image
+							img={item.homeTeam.logo}
+							alt={item.homeTeam.name}
+						/>
+						<div>{item.homeTeam.name}</div>
+					</div>
+
+					<div>Alkaa<br />{item.time}</div>
+
+					<div>
+						<Image
+							img={item.awayTeam.logo}
+							alt={item.awayTeam.name}
+						/>
+						<div>{item.awayTeam.name}</div>
+					</div>
+				</div>
 			</li>
 		{/each}
 	</ul>
